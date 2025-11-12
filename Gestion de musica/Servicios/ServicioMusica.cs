33bb46e413680d4cf919a51878ec3c938150d724
Rioluslayers101 <emmanuelproxd12@gmail.com>
2025-11-12 10:34:27 -0600

    Servicios agregado

---
+++ Gestion de musica/Servicios/ServicioMusica.cs
@@ -0,0 +1,31 @@
+﻿
+using MusicManager.Models
+namespace MusicManager.Sevice
+{
+    public class ServicioMusica
+    {
+        //Atributos
+        public GestorCanciones Gestor { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+
+        //Constructor
+        public ServicioMusica(GestorCanciones gestor) 
+        {
+            Gestor = gestor;
+            Usuarios = new List<Usuario>();
+        }
+
+        //Metodos
+        public void RegistrarUsuario(string nombre)
+        {
+            Usuario nuevoUsuario = new Usuario(nombre);
+            Usuarios.Add(nuevoUsuario);
+            Console.WriteLine($"Usuario '{nombre}' registrado exitosamente.");
+        }
+
+        public Usuario BuscarUsuario(string nombre)
+        {
+            return Usuarios.FirstOrDefault(p => p.Nombre.ToLower() == nombre.ToLower());
+        }
+    }
+}
