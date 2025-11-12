1f907116db3cbcbc6297f3f733701f8a3079b8df
Rioluslayers101 <emmanuelproxd12@gmail.com>
2025-11-12 11:43:50 -0600

    Primera parte Programa Principal

--- Gestion de musica/Program.cs
+++ Gestion de musica/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MusicManager.Models;
+using MusicManager.Sevice;
+
+
+// 1- Inicialización
+GestorCanciones gestor = new GestorCanciones();
+ServicioMusica servicioMusica = new ServicioMusica(gestor);
+
+gestor.AgregarCancion(new Cancion("Amor Eterno", "Rocío Dúrcal", 342));
+gestor.AgregarCancion(new Cancion("El Rey", "Vicente Fernández", 182));
+gestor.AgregarCancion(new Cancion("La Bikina", "Luis Miguel", 175));
+gestor.AgregarCancion(new Cancion("Ojalá que te mueras", "Panteón Rococó", 224));
+gestor.AgregarCancion(new Cancion("Como la flor", "Selena", 193));
+gestor.AgregarCancion(new Cancion("La ingrata", "Café Tacvba", 201));
+gestor.AgregarCancion(new Cancion("Eres", "Café Tacvba", 258));
+gestor.AgregarCancion(new Cancion("Si nos dejan", "Luis Miguel", 156));
+
+
+// 2- Registro Usuario
+Console.WriteLine("--- REGISTRO DE USUARIO ---");
+Console.Write("Por favor, ingrese su nombre de usuario: ");
+string nombreUsuario = Console.ReadLine();
+servicioMusica.RegistrarUsuario(nombreUsuario);
+Console.WriteLine($"\n¡Bienvenido, {nombreUsuario}!");
+
+
+//3- Creación Lista
+Console.WriteLine("\n--- CREACIÓN DE LISTA DE REPRODUCCIÓN ---");
+Console.Write("Ingrese un nombre para su primera lista de reproducción: ");
+string nombreLista = Console.ReadLine();
+usuario.CrearListaReproduccion(nombreLista);
+Console.WriteLine($"Lista '{nombreLista}' creada.");
+
+
+// 4- Menu Principal
+bool salir = false;
+
+while (!salir)
+{
+    Console.WriteLine("\n--- MENÚ PRINCIPAL ---");
+    Console.WriteLine($"Usuario actual: {usuario.Nombre}");
+    Console.WriteLine($"Lista actual: '{nombreLista}' ({CancionesDisponibles.Count} canciones)");
+    Console.WriteLine("1. Buscar canciones para agregar a mi lista");
+    Console.WriteLine("2. Ver mi lista de reproducción (ordenada por duración)");
+    Console.WriteLine("3. Ver todas las canciones disponibles");
+    Console.WriteLine("4. Crear nueva lista de reproducción");
+    Console.WriteLine("5. Cambiar de lista actual");
+    Console.WriteLine("6. Salir");
+    Console.Write("Seleccione una opción: ");
+
+    string opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+            
+            break;
+        case "2":
+           
+            break;
+        case "3":
+        
+            break;
+        case "4":
+      
+            break;
+        case "5":
+        
+            break;
+        case "6":
+            salir = true;
+            break;
+        default:
+            Console.WriteLine("Opción no válida. Intente nuevamente.");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
